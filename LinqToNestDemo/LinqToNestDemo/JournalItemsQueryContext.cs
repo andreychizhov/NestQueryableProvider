62f70a0b351d6aaac50052b3287b2dfc5d2794b3
Andrey Chizhov <ChizhovAG@ugsk.ru>
2015-10-26 19:21:26 +0600

    QueryContext class added.

---
+++ LinqToNestDemo/LinqToNestDemo/JournalItemsQueryContext.cs
@@ -0,0 +1,38 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqToNestDemo
+{
+    public class JournalItemsQueryContext
+    {
+        internal static object Execute(Expression expression, Uri node)
+        {
+            var inspector = new PredicateInspector();
+
+            SearchRequest sr = inspector.CreateRequestFromExpression(expression);
+
+            return GetJournalItemsFromElastic(node, sr);
+            
+        }
+
+        internal static IEnumerable<UAutoContractJournalItem> GetJournalItemsFromElastic(Uri node, SearchRequest request)
+        {
+            var settings = new ConnectionSettings(
+                node,
+                defaultIndex: "uauto");
+
+
+            var client = new ElasticClient(settings);
+
+
+            var searchResult = client.Search<UAutoContractJournalItem>(request);
+
+            return searchResult.Documents;
+        }
+    }
+}
